5b8071ccdc75d2e2924553a35bdf36290dade99d
Ambrusecoding <juanestebancarvallocabrera@gmail.com>
2025-11-24 17:35:06 -0500

    Todo en funcionamiento rama local origina

--- backend/src/prisma/prisma.module.ts
+++ backend/src/prisma/prisma.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
+<<<<<<< HEAD
 @Module({
   // Exportamos el servicio para que otros módulos puedan inyectarlo.
+=======
+@Global()
+@Module({
+>>>>>>> 2d4e546 (Todo en funcionamiento rama local origina)
   providers: [PrismaService],
   exports: [PrismaService],
 })
